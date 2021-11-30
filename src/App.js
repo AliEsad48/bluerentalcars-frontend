d3313b838e6e48369dac76657c4a8f5fb32fad86
Salih Bayram <alicamci2020@gmail.com>
2021-11-30 21:56:03 +0300

    V1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TopBar from "./components/common/TopBar"
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/common/TopBar.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Col, Container, Row } from "react-bootstrap"
+
+const TopBar = () => {
+  return (
+    <Container className="topbar">
+      <Row>
+        <Col>CALL US +1 235 98 95</Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default TopBar
