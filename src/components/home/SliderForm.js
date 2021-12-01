28cbf18914821704d483c7d3473aa9fcb500e3e9
Salih Bayram <alicamci2020@gmail.com>
2021-12-01 22:17:49 +0300

    V4

--- src/components/home/SliderForm.js
+++ src/components/home/SliderForm.js
@@ -1,124 +1,49 @@
 import React from "react"
 import { Button, Card, Form, FormControl, InputGroup } from "react-bootstrap"
-import { HiOutlineLocationMarker } from "react-icons/hi"
+import { FiCalendar, FiMapPin } from "react-icons/fi"
 import { MdDateRange } from "react-icons/md"
 
 const SliderForm = () => {
   return (
-    <Card style={{ alignItems: "center", marginTop: "5rem" }}>
-      <Card.Body>
-        <Form>
-          <InputGroup>
-            <Form.Select
-              aria-label="Default select example"
-              style={{
-                width: "25rem",
-              }}
-            >
-              <option>Select a car</option>
-              <option value="1">Bir</option>
-              <option value="2">İki</option>
-              <option value="3">Üç</option>
-            </Form.Select>
-          </InputGroup>
-          <InputGroup
-            style={{
-              width: "25rem",
-              marginTop: "1rem",
-            }}
-          >
-            <InputGroup.Text
-              style={{ height: "1.5rem", textAlign: "center", width: "6.2rem" }}
-            >
-              <HiOutlineLocationMarker />
-              Pick Up
-            </InputGroup.Text>
-            <FormControl
-              style={{ height: "1.5rem" }}
-              placeholder="Type a place"
-            />
-          </InputGroup>
-          <InputGroup
-            style={{
-              width: "25rem",
-              marginTop: "1rem",
-            }}
-          >
-            <InputGroup.Text
-              style={{ height: "1.5rem", textAlign: "center", width: "6.2rem" }}
-            >
-              <HiOutlineLocationMarker />
-              Drop Off
-            </InputGroup.Text>
-            <FormControl
-              style={{ height: "1.5rem" }}
-              placeholder="Type a place"
-            />
-          </InputGroup>
-          <InputGroup
-            style={{
-              width: "25rem",
-              marginTop: "1rem",
-            }}
-          >
-            <InputGroup.Text
-              style={{
-                height: "1.5rem",
-                textAlign: "center",
-                width: "6.2rem",
-              }}
-            >
-              <MdDateRange />
-              Pick Up
-            </InputGroup.Text>
-            <FormControl
-              type="date"
-              style={{
-                height: "1.5rem",
-              }}
-            />
-            <FormControl
-              type="time"
-              style={{
-                height: "1.5rem",
-              }}
-            />
-          </InputGroup>
-          <InputGroup
-            style={{
-              width: "25rem",
-              marginTop: "1rem",
-            }}
-          >
-            <InputGroup.Text
-              style={{
-                height: "1.5rem",
-                textAlign: "center",
-                width: "6.2rem",
-              }}
-            >
-              <MdDateRange />
-              Drop Off
-            </InputGroup.Text>
-            <FormControl
-              type="date"
-              style={{
-                height: "1.5rem",
-              }}
-            />
-            <FormControl
-              type="time"
-              style={{
-                height: "1.5rem",
-              }}
-            />
-          </InputGroup>
-        </Form>
-        <Button style={{ width: "25rem", height: "3rem", marginTop: "1rem" }}>
-          CONTINUE RESERVATION
-        </Button>
-      </Card.Body>
-    </Card>
+    <Form>
+      <Form.Select size="lg" className="mb-3">
+        <option>Select a car</option>
+      </Form.Select>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">
+          <FiMapPin />
+          &nbsp; Pick up
+        </InputGroup.Text>
+        <FormControl placeholder="Type a place" />
+      </InputGroup>
+
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">
+          <FiMapPin />
+          &nbsp;Drop off
+        </InputGroup.Text>
+        <FormControl placeholder="Type a place" />
+      </InputGroup>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">
+          <FiCalendar />
+          &nbsp; Pick up
+        </InputGroup.Text>
+        <FormControl type="date" />
+        <FormControl type="time" />
+      </InputGroup>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">
+          <FiCalendar />
+          &nbsp;Drop off
+        </InputGroup.Text>
+        <FormControl type="date" />
+        <FormControl type="time" />
+      </InputGroup>
+      <Button size="lg" className="w-100" type="submit">
+        CONTINUE RESERVATION
+      </Button>
+    </Form>
   )
 }
 
