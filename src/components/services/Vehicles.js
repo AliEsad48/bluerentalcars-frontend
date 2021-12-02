c5540f5f19222e2f1237429795bb5f39d9066b19
Salih Bayram <alicamci2020@gmail.com>
2021-12-02 23:18:16 +0300

    V7

--- src/components/services/Vehicles.js
+++ src/components/services/Vehicles.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react"
-import { Button, Col, Container, Image, Row } from "react-bootstrap"
-import { FiChevronDown, FiChevronUp } from "react-icons/fi"
+import { Button, Col, Container, Image, Row, Table } from "react-bootstrap"
+import { FiCheck, FiChevronDown, FiChevronUp, FiX } from "react-icons/fi"
 import SectionTitle from "../common/SectionTitle"
 import { vehicleList } from "../../data/vehicleList"
 
 const Vehicles = () => {
   const [vehicles, setVehicles] = useState(vehicleList)
   const [activeVehicle, setActiveVehicle] = useState(0)
+  const [startIndex, setStartIndex] = useState(0)
+  const vehiclesLength = 5
 
   return (
     <Container>
@@ -19,15 +21,20 @@ const Vehicles = () => {
                 <FiChevronUp />
               </Button>
             </li>
-            {vehicles.map((vehicle, index) => (
-              <li
-                key={vehicle.id}
-                className={index === activeVehicle ? "active" : ""}
-                onClick={() => setActiveVehicle(index)}
-              >
-                {vehicle.model}
-              </li>
-            ))}
+            {vehicles.map((vehicle, index) => {
+              if (index >= 15) {
+                return (
+                  <li
+                    key={vehicle.id}
+                    className={index === activeVehicle ? "active" : ""}
+                    onClick={() => setActiveVehicle(index)}
+                  >
+                    {vehicle.model}
+                  </li>
+                )
+              }
+              return null
+            })}
 
             <li>
               <Button>
@@ -42,7 +49,58 @@ const Vehicles = () => {
             className="img-fluid"
           />
         </Col>
-        <Col lg={3}>DETAYLAR</Col>
+        <Col lg={3}>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th colSpan={2}>
+                  <h3>${vehicles[activeVehicle].pricePerDay} per day</h3>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Model</td>
+                <td>{vehicles[activeVehicle].model}</td>
+              </tr>
+              <tr>
+                <td>Doors</td>
+                <td>{vehicles[activeVehicle].doors}</td>
+              </tr>
+              <tr>
+                <td>Seats</td>
+                <td>{vehicles[activeVehicle].seats}</td>
+              </tr>
+              <tr>
+                <td>Luggage</td>
+                <td>{vehicles[activeVehicle].luggage}</td>
+              </tr>
+              <tr>
+                <td>Transmission</td>
+                <td>{vehicles[activeVehicle].transmission}</td>
+              </tr>
+              <tr>
+                <td>Air Conditioning</td>
+                <td>
+                  {vehicles[activeVehicle].airConditioning ? (
+                    <FiCheck />
+                  ) : (
+                    <FiX />
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <td>Fuel Type</td>
+                <td>{vehicles[activeVehicle].fuelType}</td>
+              </tr>
+
+              <tr>
+                <td>Age</td>
+                <td>{vehicles[activeVehicle].age}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
       </Row>
     </Container>
   )
